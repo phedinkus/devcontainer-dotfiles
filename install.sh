b6fd35582001f2bf86cb4f1332555889cf02f6d6
Katie Miller <phedinkus@gmail.com>
2025-03-30 12:20:45 +0200

    Try a different way

--- install.sh
+++ install.sh
@@ -1,47 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-install_zsh() {
-  if ! command -v zsh >/dev/null 2>&1; then
-    echo "zsh is not installed. Please install zsh and rerun this script."
-    exit 1
-  fi
+# powerline fonts for zsh agnoster theme
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd .. && rm -rf fonts
 
-  # Get the path of zsh
-  ZSH_PATH=$(which zsh)
+# oh-my-zsh & plugins
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+cp ./.zshrc ~
 
-  PLUGINS_DIR="$HOME/.zsh_addons"
-  # Get the directory of the script
-  SCRIPT_DIR=$(dirname "$(realpath "$0")")
-  # copies dotfiles
+########################################################################################################################
+#### set agnoster as theme, this came from https://gist.github.com/corentinbettiol/21a6d4e942a0ee58d51acb7996697a88
+#### in vscode settings for devcontainer (not for User or Workspace), Search for terminal.integrated.fontFamily value, and set it to "Roboto Mono for Powerline" (or any of those: https://github.com/powerline/fonts#font-families font families).
+# save current zshrc
+mv ~/.zshrc ~/.zshrc.bak
 
-  # Creates root directories if they don't exist
-  mkdir -p "$HOME/.config"
-  mkdir -p "$PLUGINS_DIR"
+sudo sh -c "$(wget -O- https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)" -- \
+    -t agnoster
 
-  # Create symlinks for the dotfiles
-  ln -s "$SCRIPT_DIR/.zshrc" "$HOME/.zshrc"
-
-  # Create a symlink for the starship configuration
-  git clone https://github.com/zsh-users/zsh-autosuggestions $PLUGINS_DIR/zsh-autosuggestions
-  git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $PLUGINS_DIR/zsh-autocomplete
-  git clone https://github.com/zdharma-continuum/fast-syntax-highlighting $PLUGINS_DIR/fast-syntax-highlighting
-
-  # Change the default shell to zsh
-  sudo chsh -s "$ZSH_PATH"
-
-  # Check if the shell was changed successfully
-  if [ $? -eq 0 ]; then
-    echo "Successfully changed the default shell to zsh."
-    echo "Please log out and log back in for the changes to take effect."
-  else
-    echo "Failed to change the default shell."
-    exit 1
-  fi
-}
-
-
-main() {
-  install_zsh
-}
-
-main
+# remove newly created zshrc
+rm -f ~/.zshrc
+# restore saved zshrc
+mv ~/.zshrc.bak ~/.zshrc
+# update theme
+sed -i '/^ZSH_THEME/c\ZSH_THEME="agnoster"' ~/.zshrc
+# update plugins
+sed -i '/^plugins/c\plugins=(git rails ruby zsh-syntax-highlighting zsh-autosuggestions)' ~/.zshrc
+########################################################################################################################
